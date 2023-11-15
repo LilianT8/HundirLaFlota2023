d879c688bfd7d5742cbee60070f87c2245cf2eaf
LilianT8 <118301092+LilianT8@users.noreply.github.com>
2023-11-15 13:13:31 +0100

    Update HundirFlota.java
    desde casa a clase

--- src/HundirFlota.java
+++ src/HundirFlota.java
@@ -1,98 +1,119 @@
-// tablerojugador/ tablero jugador disparos
-// pc // pc
-// DE 0 A 9gf
-// TABLERO JUGADFOR Y TABLERO DISPARO JUGADOR
 public class HundirFlota {
     public static void main(String[] args) {
         char[][] tableroJugador = new char[10][10];
         char[][] tableroPc = new char[10][10];
-        char[][] tableroDiparosJugador = new char[10][10];
+        char[][] tableroDisparosJugador = new char[10][10];
         char[][] tableroDisparosPc = new char[10][10];
-        int []  barcos = {4, 3, 3, 2, 2, 1, 6};
-        char[] letras = {'A','B', 'C', 'D', 'E' , 'F', 'G', 'H','I','J' };
+        int[] barcos = {4, 3, 3, 2, 2, 1, 6};
+        char[] letras = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
         rellenarTableroJugador(tableroJugador);
         rellenarTableroPc(tableroPc);
+        rellenarTableroDisparosJugador(tableroDisparosJugador);
+        rellenarTableroDisparosPc(tableroDisparosPc);
 
         System.out.println();
-        mostrarTableroJugador(tableroJugador,letras, tableroDiparosJugador);
+        mostrarTablero(tableroJugador, letras, tableroDisparosJugador);
         System.out.println();
-        mostrarTableroPc(tableroPc);
+        mostrarTableroPc(tableroPc, tableroDisparosPc, letras);
+    }
 
+    public static void mostrarTablero(char[][] tableroJugador, char[] letras, char[][] tableroDisparos) {
+        System.out.println("    TABLERO JUGADOR          TABLERO DISPAROS");
+        System.out.print("  ");
+        for (char letra : letras) {
+            System.out.print(letra + " ");
+        }
+        System.out.print("     ");
+        for (char letra : letras) {
+            System.out.print(letra + " ");
+        }
+        System.out.println(); // Aquí ya se han impreso las letras y empieza a imprimir número de filas
+
+        for (int i = 0; i < tableroJugador.length; i++) {
+            System.out.print(i + " "); // Imprime número 0 va para abajo e imprime virguilillas de la a a la j por filas despues espcio igual
+
+            // Mostrar tablero del jugador
+            for (int j = 0; j < tableroJugador[i].length; j++) {
+                System.out.print(tableroJugador[i][j] + " ");
+            }
+
+            System.out.print("     ");
+
+            // Mostrar tablero de disparos del jugador
+            for (int k = 0; k < tableroDisparos[i].length; k++) {
+                System.out.print(tableroDisparos[i][k] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+
+
+    public static void mostrarTableroPc(char[][] tableroPc, char[][] tableroDisparosPc , char [] letras) {
+        System.out.println("       TABLERO PC              TABLERO DISPAROS PC");
+        System.out.print("  ");
+        for (char letra : letras) {
+            System.out.print(letra + " ");
+        }
+        System.out.print("         ");
+        for (char letra : letras) {
+            System.out.print(letra + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < tableroPc.length; i++) {
+            System.out.print(i + " "); // Números de fila
+
+            // Mostrar tablero PC
+            for (int j = 0; j < tableroPc[i].length; j++) {
+                System.out.print(tableroPc[i][j] + " ");
+            }
+
+            System.out.print("         ");
+
+            // Mostrar tablero de disparos PC
+            for (int k = 0; k < tableroDisparosPc[i].length; k++) {
+                System.out.print(tableroDisparosPc[i][k] + " ");
+            }
+
+            System.out.println();
+        }
     }
 
 
     public static void rellenarTableroJugador(char[][] tableroJugador) {
         for (int i = 0; i < tableroJugador.length; i++) {
-            for (int j = 0; j < tableroJugador.length; j++) {
+            for (int j = 0; j < tableroJugador[i].length; j++) {
                 tableroJugador[i][j] = '~';
             }
-
         }
-
     }
 
     public static void rellenarTableroPc(char[][] tableroPc) {
-
         for (int i = 0; i < tableroPc.length; i++) {
-            for (int j = 0; j < tableroPc.length; j++) {
+            for (int j = 0; j < tableroPc[i].length; j++) {
                 tableroPc[i][j] = '~';
-
             }
-
         }
     }
 
-    public static void mostrarTableroJugador(char[][] tableroJugador, char[] letras, char [][] tableroDisparosJugador) {
-        System.out.println("  TABLERO JUGADOR  ");
-        System.out.print("  ");
-        for (char letra : letras) {
-            System.out.print(letra + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < tableroJugador.length; i++) {
-            System.out.print(i + 1 + " "); // Números de fila
-            for (int j = 0; j < tableroJugador[i].length; j++) {
-                System.out.print(tableroJugador[i][j] + " ");
+    public static void rellenarTableroDisparosJugador(char[][] tableroDisparosJugador) {
+        for (int i = 0; i < tableroDisparosJugador.length; i++) {
+            for (int j = 0; j < tableroDisparosJugador[i].length; j++) {
+                tableroDisparosJugador[i][j] = '~';
             }
-            System.out.print("   ");
-            for (int g = 0;  g<tableroDisparosJugador.length; g++) {
-                for (int k = 0; k < tableroDisparosJugador.length; k++) {
-                 //   System.out.print(tableroDisparosJugador[g][k] + " ");
-                }
-            }
-            System.out.println();
         }
     }
 
-
-    public static void mostrarTableroPc(char[][] tableroPc) {
-        System.out.println("    TABLERO PC  ");
-        System.out.println("1 2 3 4 5 6 7 8 9 10");
-        for (int i = 0; i < tableroPc.length; i++) {
-            for (int j = 0; j < tableroPc.length; j++) {
-                System.out.print(tableroPc[i][j] + " ");
+    public static void rellenarTableroDisparosPc(char[][] tableroDisparosPc) {
+        for (int i = 0; i < tableroDisparosPc.length; i++) {
+            for (int j = 0; j < tableroDisparosPc[i].length; j++) {
+                tableroDisparosPc[i][j] = '~';
             }
-            System.out.println();
         }
     }
-
-    public static void rellenarTableroDisparosJugador (char[][] tableroDisparosJugador){
-        for (int i = 0; i <tableroDisparosJugador.length; i++) {
-            for (int j = 0; j < tableroDisparosJugador.length; j++) {
-                tableroDisparosJugador [i] [j] = '~';
-            }
-
-        }
-
-    }
-
-
-
-
-
-
 
 }
-
 
